0ab772baf708e7ef53a694a068558aea7d123128
“Miray <miray0853@gmail.com>
2026-03-19 19:11:21 +0300

    .

--- obj/Debug/net10.0/PacMan2.AssemblyInfo.cs
+++ obj/Debug/net10.0/PacMan2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PacMan2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df1d13e51d55d7b23fcaed7b2e8ba83b979403d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c643025f2778a2edcaa8ba1aa92952c385599bd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PacMan2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PacMan2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
